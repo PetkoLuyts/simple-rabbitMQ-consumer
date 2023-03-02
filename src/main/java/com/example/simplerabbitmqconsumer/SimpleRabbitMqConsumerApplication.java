9cbbf968f6f080aabc32476fef653666d94a6244
petko.lyutskanov <simona2000petko@abv.bg>
2023-03-02 10:08:11 +0200

    Initial commit

---
+++ src/main/java/com/example/simplerabbitmqconsumer/SimpleRabbitMqConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplerabbitmqconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRabbitMqConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleRabbitMqConsumerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simplerabbitmqconsumer/SimpleRabbitMqConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplerabbitmqconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRabbitMqConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
